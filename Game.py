e02196a43eeccaa27ff799955d35c73cb839fb0b
h.seok <h.seoK@samsung.com>
2024-07-10 14:07:24 +0900

    [feature] #2 param length is not 3

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
 class Game:
-    def guess(self, param):
+    def guess(self, param: str):
         if param == None:
             raise ValueError("Arg must be List")
+        if len(param) != 3:
+            raise ValueError("Arg must be 3 digits")
--- test_Game.py
+++ test_Game.py
@@ -7,7 +7,13 @@ class TestGame(TestCase):
     def setUp(self):
         super().setUp()
         self.sut = Game()
+
     def test_raise_exception_when_input_is_none(self):
         with self.assertRaises(ValueError) as ve:
             self.sut.guess(None)
         self.assertEqual("Arg must be List", str(ve.exception))
+
+    def test_raise_exception_when_input_length_is_not_proper(self):
+        with self.assertRaises(ValueError) as ve:
+            self.sut.guess("12")
+        self.assertEqual("Arg must be 3 digits", str(ve.exception))
