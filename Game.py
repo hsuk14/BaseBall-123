a349d4829c3dc38fa218e75f2b051336846ed5f4
h.seok <h.seoK@samsung.com>
2024-07-10 14:11:47 +0900

    [refactoring] #2

--- Game.py
+++ Game.py
@@ -1,5 +1,8 @@
 class Game:
     def guess(self, param: str):
+        self.validation_of_arg(param)
+
+    def validation_of_arg(self, param):
         if param == None:
             raise ValueError("Arg must be List")
         if len(param) != 3:
--- test_Game.py
+++ test_Game.py
@@ -9,11 +9,13 @@ class TestGame(TestCase):
         self.sut = Game()
 
     def test_raise_exception_when_input_is_none(self):
-        with self.assertRaises(ValueError) as ve:
-            self.sut.guess(None)
-        self.assertEqual("Arg must be List", str(ve.exception))
+        self.assert_illegal_arg(None, "Arg must be List")
 
     def test_raise_exception_when_input_length_is_not_proper(self):
+        self.assert_illegal_arg("12", "Arg must be 3 digits")
+
+    def assert_illegal_arg(self, input_word, expected_result):
         with self.assertRaises(ValueError) as ve:
-            self.sut.guess("12")
-        self.assertEqual("Arg must be 3 digits", str(ve.exception))
+            self.sut.guess(input_word)
+        self.assertEqual(expected_result, str(ve.exception))
+
